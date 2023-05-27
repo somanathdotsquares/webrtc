bd1f7e33fb1932e5f5eb1a5cc596cded751de27a
somanathdotsquares <somanath.gaikwad@dotsquares.com>
2023-05-27 15:26:06 +0530

    this is upto guest user 27 may

--- src/App.js
+++ src/App.js
@@ -4,21 +4,52 @@ import Home from "./Pages/Home/Home";
 import Navigation from "./components/shared/Navigation/Navigation";
 import Register from "./Pages/Register/Register";
 import Login from "./Pages/Login/Login";
+import Authenticate from "./Pages/Authenticate/Authenticate";
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        
-          <Route path="/" exact element={<Home />} />
-          
-          <Route path="/register" element={<Register />} />
+        <Route path="/" exact element={<Home />} />
 
-          <Route path="/login" element={<Login></Login>} />
+        {/* <Route path="/authenticate"  element={<Authenticate/>} /> */}
+
+
+        <GuestRoute path="/authenticate" >
+        <Authenticate/>
+
+        </GuestRoute>
+        
+        {/* <Route path="/register" element={<Register />} />
+
+          <Route path="/login" element={<Login></Login>} /> */}
       </Routes>
     </BrowserRouter>
   );
 }
+
+// create componetnt
+const GuestRoute = ({ children, ...rest }) => {
+  const { isAuth } = useSelector((state) => state.auth);
+  return (
+      <Route
+          {...rest}
+          render={({ location }) => {
+              return isAuth ? (
+                  <Redirect
+                      to={{
+                          pathname: '/rooms',
+                          state: { from: location },
+                      }}
+                  />
+              ) : (
+                  children
+              );
+          }}
+      ></Route>
+  );
+};
+
 
 export default App;
