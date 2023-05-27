f8c691faa4bf20bcd4f53eb89604880a327748ce
somanathdotsquares <somanath.gaikwad@dotsquares.com>
2023-05-27 14:37:02 +0530

    new branch message2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -28,7 +28,8 @@ const Home = () => {
     <p className={styles.text}>
         In publishing and graphic design, Lorem ipsum is a placeholder text
         commonly used to demonstrate the visual form of a document or a typeface
-        without relying on meaningful content.
+        without relying on meaningful content 1.
+     
       </p>
       <div>
       
